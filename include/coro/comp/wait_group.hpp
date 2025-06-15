c3fac6dc3d71c480811a28bf646684ce7e787c95
xRay2016 <1150722393@qq.com>
2025-06-15 17:50:43 +0800

    lab_4c

--- include/coro/comp/wait_group.hpp
+++ include/coro/comp/wait_group.hpp
@@ -13,13 +13,14 @@
 #include <atomic>
 #include <coroutine>
 
+#include "coro/context.hpp"
 #include "coro/detail/types.hpp"
 
 namespace coro
 {
 /**
  * @brief Welcome to tinycoro lab4c, in this part you will build the basic coroutine
- * synchronization component——wait_group by modifing wait_group.hpp and wait_group.cpp.
+ * synchronization componentï¿½ï¿½wait_group by modifing wait_group.hpp and wait_group.cpp.
  * Please ensure you have read the document of lab4c.
  *
  * @warning You should carefully consider whether each implementation should be thread-safe.
@@ -34,20 +35,42 @@ namespace coro
  */
 
 class context;
-
+using std::atomic;
 // TODO[lab4c]: This wait_group is an example to make complie success,
 // You should delete it and add your implementation, I don't care what you do,
 // but keep the member function and construct function's declaration same with example.
 class wait_group
 {
 public:
-    explicit wait_group(int count = 0) noexcept {}
+    struct awaiter
+    {
+        awaiter(context& ctx, wait_group& wg) noexcept : m_ctx(ctx), m_wg(wg) {};
 
-    auto add(int count) noexcept -> void {};
+        auto await_ready() noexcept -> bool;
+        auto await_suspend(std::coroutine_handle<> handle) noexcept -> bool;
+        auto await_resume() noexcept -> void;
 
-    auto done() noexcept -> void {};
+        context&                m_ctx;
+        wait_group&             m_wg;
+        std::coroutine_handle<> m_handle{nullptr};
+        awaiter*                m_next{nullptr};
+    };
 
-    auto wait() noexcept -> detail::noop_awaiter { return {}; };
+    explicit wait_group(int count = 0) noexcept : m_count(count) {}
+
+    auto add(int count) noexcept -> void;
+
+    auto done() noexcept -> void;
+
+    auto wait() noexcept -> awaiter { return awaiter{local_context(), *this}; };
+
+private:
+    auto resume_all() noexcept -> void;
+    auto register_awaiter(awaiter* awaiter) noexcept -> bool;
+
+private:
+    atomic<int>                 m_count;
+    atomic<detail::awaiter_ptr> m_state{nullptr};
 };
 
 }; // namespace coro
--- src/comp/wait_group.cpp
+++ src/comp/wait_group.cpp
@@ -4,4 +4,60 @@
 namespace coro
 {
 // TODO[lab4c] : Add codes if you need
+auto wait_group::add(int count) noexcept -> void
+{
+    m_count.fetch_add(count, memory_order_acq_rel);
+}
+auto wait_group::done() noexcept -> void
+{
+    if (m_count.fetch_sub(1, memory_order_acq_rel) <= 1)
+    {
+        resume_all();
+    }
+}
+auto wait_group::resume_all() noexcept -> void
+{
+    auto ptr = static_cast<awaiter*>(m_state.exchange(nullptr, memory_order_acq_rel));
+    while (ptr != nullptr)
+    {
+        auto next_awaiter = ptr->m_next;
+        ptr->m_ctx.submit_task(ptr->m_handle);
+        ptr = next_awaiter;
+    }
+}
+auto wait_group::register_awaiter(awaiter* aw) noexcept -> bool
+{
+    // set awaiter to list
+    detail::awaiter_ptr old_head = nullptr;
+    while (true)
+    {
+        // if already zero wait, return false(not suspend)
+        if (m_count.load(memory_order_acquire) <= 0)
+        {
+            return false;
+        }
+
+        old_head   = m_state.load(std::memory_order_acquire);
+        aw->m_next = static_cast<awaiter*>(old_head);
+        if (m_state.compare_exchange_weak(old_head, static_cast<detail::awaiter_ptr>(aw), memory_order_acq_rel))
+        {
+            return true;
+        }
+    }
+}
+
+auto wait_group::awaiter::await_ready() noexcept -> bool
+{
+    m_ctx.register_wait();
+    return m_wg.m_count.load(memory_order_acquire) <= 0;
+}
+auto wait_group::awaiter::await_suspend(std::coroutine_handle<> handle) noexcept -> bool
+{
+    m_handle = handle;
+    return m_wg.register_awaiter(this);
+}
+auto wait_group::awaiter::await_resume() noexcept -> void
+{
+    return m_ctx.unregister_wait();
+}
 }; // namespace coro
